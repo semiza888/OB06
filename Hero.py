fba9a46b303662be4ab4bd817daa00383e0b619a
semiza888 <164627267+semiza888@users.noreply.github.com>
2024-04-21 17:22:52 +0300

    add a new code

--- Hero.py
+++ Hero.py
@@ -5,8 +5,11 @@ class Hero:
         self.attack_power = attack_power
 
     def attack(self, other):
-        other.health -= self.attack_power
-        print(f"{self.name} нанес {self.attack_power} урона {other.name}.")
+        # Реализация случайного выбора силы атаки
+        attack_power = random.randint(self.attack_power - 5, self.attack_power + 5)
+        # Проверка, что уровень здоровья противника не опустился ниже 0
+        other.health = max(0, other.health - attack_power)
+        print(f"{self.name} нанес {attack_power} урона {other.name}.")
         print(f"У {other.name} осталось {other.health} здоровья.")
 
     def is_alive(self):
